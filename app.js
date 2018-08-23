d976e86ad6049269ab14f1234b7ce3ab577e0f53
webjazznl <hsiebelink73@gmail.com>
2018-08-23 21:26:13 +0200

    Serve HTML template

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ var express = require('express');
 var debug = require('debug')('app');
 var chalk = require('chalk');
 var morgan = require('morgan');
-
+var path = require('path');
 var app = express();
 
 app.use(morgan("tiny"));
 
 app.get("/", function(req, res) {
-    res.send("Hello from lib app");
+    res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
 var port = 3000;
