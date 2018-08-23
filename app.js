b54354648c00e8cfdeb64d8fe287feea80b841db
webjazznl <hsiebelink73@gmail.com>
2018-08-23 21:14:30 +0200

    Add morgan module for http log to console

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var debug = require('debug')('app');
 var chalk = require('chalk');
+var morgan = require('morgan');
 
 var app = express();
+
+app.use(morgan("tiny"));
 
 app.get("/", function(req, res) {
     res.send("Hello from lib app");
