72e77099663255efa815a232bd6c7029828de5f9
webjazznl <hsiebelink73@gmail.com>
2018-08-24 17:46:36 +0200

    Replace css and js  with versions installed by npm

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ var path = require('path');
 var app = express();
 
 app.use(morgan("tiny"));
+//these static file paths are checked by express for files in specified order...
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, 'views/index.html'));
