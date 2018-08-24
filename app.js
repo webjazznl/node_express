bbfc04692787894168e38d12cb7bd249135993cd
webjazznl <hsiebelink73@gmail.com>
2018-08-24 22:35:23 +0200

    Install eslint and run it

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
-var express = require('express');
-var debug = require('debug')('app');
-var chalk = require('chalk');
-var morgan = require('morgan');
-var path = require('path');
-var app = express();
+const express = require('express');
+const debug = require('debug')('app');
+const chalk = require('chalk');
+const morgan = require('morgan');
+const path = require('path');
 
-app.use(morgan("tiny"));
-//these static file paths are checked by express for files in specified order...
+const app = express();
+
+app.use(morgan('tiny'));
+// these static file paths are checked by express for files in specified order...
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, 'views/index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-var port = 3000;
-app.listen(port, function(){
-    debug(chalk.green(`Listening on port ${port}`));
+const port = 3000;
+app.listen(port, () => {
+  debug(chalk.green(`Listening on port ${port}`));
 });
