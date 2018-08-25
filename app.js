af82d15e9626bce68db841a8aa5ba6c8101e2329
webjazznl <hsiebelink73@gmail.com>
2018-08-25 10:22:57 +0200

    Add pug templating engine

--- app.js
+++ app.js
@@ -12,9 +12,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.set('view engine', 'pug');
+app.set('views', './src/views');
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views/index.html'));
+  res.render('index');
 });
 
 const port = process.env.PORT || 3000;
